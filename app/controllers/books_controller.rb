74cb1d6bc9a6f790244832d0c8d81ba326d5b790
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2022-12-12 08:20:52 +0000

    [Add:221212]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,18 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    
+    if @book.save
+      redirect_to books_path(@book)
+    else
+      render :new
+    end
   end
 
   def show
@@ -10,4 +22,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end

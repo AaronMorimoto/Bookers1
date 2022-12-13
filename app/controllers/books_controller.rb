3d02627850a2c4bf607d996d35f430c0b169eb44
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2022-12-13 11:47:51 +0000

    [Add 221213]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,8 +26,13 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to books_path(@book)  
+    
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+    
   end
   
   def destroy
